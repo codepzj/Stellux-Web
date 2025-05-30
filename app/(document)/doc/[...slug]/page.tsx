2ff61890cf8d29d011069ef7a91724e785930e4f
codepzj <email@codepzj.cn>
2025-05-31 01:45:33 +0800

    fix: 更新@types/node版本并重构文档页面逻辑

--- app/(document)/doc/[...slug]/page.tsx
+++ app/(document)/doc/[...slug]/page.tsx
@@ -1,105 +1,62 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
-
-import { getDocumentByID, getDocumentTreeByID, getRootDocumentByID } from "@/api/document";
+import { getDocumentByID, getRootDocumentByID } from "@/api/document";
 import { getTableOfContents, TableOfContents } from "@/lib/toc";
-import { convertToTreeData } from "@/utils/convert";
-import useDocStore from "@/store/doc";
 
 import { Markdown } from "@/components/Md";
 import { ScrollToc } from "@/components/Toc";
 import { DocumentRootVO, DocumentVO } from "@/types/document";
-import { Spinner } from "@heroui/spinner";
-
-export default function DocPage() {
-    const params = useParams();
-    const [alias, document_id, leaf_id] = params.slug as string[] || [];
-
-    const { docId, setDoctree, setDocTitle, setDocThumbnail } = useDocStore();
-    const [rootDocument, setRootDocument] = useState<DocumentRootVO | null>(null);
-    const [document, setDocument] = useState<DocumentVO | null>(null);
-    const [toc, setToc] = useState<TableOfContents>({ items: [] });
-
-    useEffect(() => {
-        if (!document_id) return;
-
-        const fetchDocTree = async () => {
-            if (docId === document_id) return; // 不重复加载文档树
-            try {
-                const res = await getDocumentTreeByID(document_id);
-                const treeItems = convertToTreeData(res.data, alias);
-                setDoctree(document_id, treeItems);
-            } catch (error) {
-                console.error("获取文档树失败:", error);
-            }
-        };
-
-        const fetchRoot = async () => {
-            if (!leaf_id) {
-                try {
-                    const res = await getRootDocumentByID(document_id);
-                    setRootDocument(res.data);
-                    setDocument(null); // 清空之前的子文档数据
-                    setDocTitle(res.data.title); // 设置文档标题
-                    setDocThumbnail(res.data.thumbnail); // 设置文档缩略图
-                } catch (error) {
-                    console.error("获取根文档失败:", error);
-                }
-            }
-        };
-
-        const fetchLeaf = async () => {
-            if (leaf_id) {
-                try {
-                    const res = await getDocumentByID(leaf_id);
-                    const doc = res.data;
-                    const toc = await getTableOfContents(doc.content);
-                    setDocument(doc);
-                    setToc(toc);
-                    setRootDocument(null); // 清空根文档数据
-                } catch (error) {
-                    console.error("获取子文档失败:", error);
-                }
-            }
-        };
-
-        fetchDocTree();
-        fetchRoot();
-        fetchLeaf();
-    }, [alias, document_id, leaf_id, docId, setDoctree]);
-
-    if (!document_id) return null;
-
-    if (!leaf_id && !rootDocument) return <Loading />;
-    if (leaf_id && !document) return <Loading />;
+import SetDocHandler from "./setDocHandler";
 
 
-    const renderTitle = leaf_id ? document?.title : rootDocument?.title;
-    const renderContent = leaf_id ? document?.content : rootDocument?.description;
+interface DocPageProps {
+    params: Promise<{ slug: string[] }>;
+}
 
+export default async function DocPage({ params }: DocPageProps) {
+    const { slug } = await params;
+    const [alias, document_id, leaf_id] = slug as string[] || [];
+
+
+    let rootDocument: DocumentRootVO | null = null;
+    let document: DocumentVO | null = null;
+    let toc: TableOfContents = { items: [] };
+
+    // 如果leaf_id为空，则获取根文档
+    if (!leaf_id) {
+        const res = await getRootDocumentByID(document_id);
+        rootDocument = res.data;
+        document = null;
+        toc = await getTableOfContents(rootDocument.description);
+        return (
+            <div className="relative w-full mx-auto text-default-600">
+                <div className="w-full flex flex-col md:flex-row justify-center gap-2">
+                    <div className="flex-1 md:max-w-xl lg:max-w-3xl md:mr-4 mb-20 px-0 md:px-4 mx-auto md:mx-0">
+                        <div className="text-3xl font-bold font-sans py-4 mb-12 text-title">{rootDocument.title}</div>
+                        <Markdown className="pl-2 md:pl-4 break-words overflow-x-auto" content={rootDocument.description || ""} />
+                        <SetDocHandler alias={alias} document_id={document_id} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    // 如果leaf_id不为空，则获取子文档
+    const res = await getDocumentByID(leaf_id);
+    document = res.data;
+    toc = await getTableOfContents(document.content);
     return (
         <div className="relative w-full mx-auto text-default-600">
             <div className="w-full flex flex-col md:flex-row justify-center gap-2">
                 <div className="flex-1 md:max-w-xl lg:max-w-3xl md:mr-4 mb-20 px-0 md:px-4 mx-auto md:mx-0">
-                    <div className="text-3xl font-bold font-sans py-4 mb-12 text-title">{renderTitle}</div>
-                    <Markdown className="pl-2 md:pl-4 break-words overflow-x-auto" content={renderContent || ""} />
+                    <div className="text-3xl font-bold font-sans py-4 mb-12 text-title">{document.title}</div>
+                    <Markdown className="pl-2 md:pl-4 break-words overflow-x-auto" content={document.content || ""} />
                 </div>
                 {leaf_id && (
                     <div className="hidden lg:block sticky top-4 h-[calc(100vh-1rem)] w-48 shrink-0">
                         <ScrollToc toc={toc} />
                     </div>
                 )}
+                <SetDocHandler alias={alias} document_id={document_id} />
             </div>
         </div>
-    );
-}
-
-function Loading() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <Spinner classNames={{ label: "text-foreground mt-4" }} label="加载中..." variant="wave" />
-        </div>
-    );
+    )
 }
--- components/Sidebar/doc-title.tsx
+++ components/Sidebar/doc-title.tsx
@@ -1,13 +1,14 @@
 "use client"
 
-import * as React from "react"
-
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+import { ChevronsUpDown, GalleryVerticalEnd } from "lucide-react"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 export function DocTitle({
   thumbnail,
@@ -16,21 +17,39 @@ export function DocTitle({
   thumbnail?: string;
   title: string;
 }) {
-
+  const router = useRouter();
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-       
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                {thumbnail && <Image className="w-full h-full object-cover bg-white dark:bg-black rounded-xs border-0.5 border-default-500" src={thumbnail} alt={title} width={32} height={32} />}
+                {thumbnail ? <Image className="w-full h-full object-cover bg-white dark:bg-black rounded-xs border-0.5 border-default-500" src={thumbnail} alt={title} width={32} height={32} /> : <GalleryVerticalEnd className="size-4" />}
               </div>
               <div className="flex flex-col gap-0.5 leading-none">
-                <span className="font-medium">{title}</span>
+                <span className="font-medium cursor-pointer">{title}</span>
               </div>
+              <ChevronsUpDown className="ml-auto" />
             </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width)"
+            align="start"
+          >
+            <DropdownMenuItem>
+              <div className="w-full cursor-pointer" onClick={() => {
+                router.push("/doc");
+                router.refresh();
+              }}>
+                退出文档
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
   )
